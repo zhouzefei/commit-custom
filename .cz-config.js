9292a5ea30efa0352532ca274f07b02d8a2660c0
周泽飞 <zefei.zhou@tongdun.net>
2022-03-17 16:33:56 +0800

    feat(components): iuuuu

--- .cz-config.js
+++ .cz-config.js
@@ -25,7 +25,7 @@ module.exports = {
     // allowCustomScopes: true,
     // allowBreakingChanges: [], //['feat', 'fix'],
     // skip any questions you want
-    skipQuestions: ['scope', 'body', 'footer'], //'confirmCommit ',
+    skipQuestions: ['scope', 'body', 'footer'], //'confirmCommit',
   
     // subject 限制长度
     subjectLimit: 100,
