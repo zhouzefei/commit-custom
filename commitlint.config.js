fa6731f9de03f02b6ad81ebfd0c588c3d89b53e6
周泽飞 <zefei.zhou@tongdun.net>
2022-03-20 14:45:31 +0800

    feat:

--- commitlint.config.js
+++ commitlint.config.js
@@ -14,10 +14,10 @@ module.exports = {
         ],
         'type-case': [0],
         'type-empty': [0],
-        'scope-empty': [0, 'never'],
-        'scope-case': [0, 'never'],
+        'scope-empty': [0],
+        'scope-case': [0],
         'subject-full-stop': [0, 'never'],
         'subject-case': [0, 'never'],
-        'header-max-length': [0, 'always', 100]
+        'header-max-length': [0, 'always', 72]
       }
 };
